394a906881d4101df48b81c83f582f56279daadd
Malkuthe <jp.baltao@gmail.com>
2014-06-10 19:31:08 -0500

    Updated BattleClassMod Created mod info file

--- battleclassmod/BattleClassMod.java
+++ battleclassmod/BattleClassMod.java
@@ -1,5 +1,30 @@
 package battleclassmod;
 
-public class BattleClassMod {
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
 
+@Mod( modid = BCMInfo.ID, name = BCMInfo.NAME, version = BCMInfo.VERSION )
+@NetworkMod( channels = BCMInfo.CHANNEL, clientSideRequired = true, serverSideRequired = true )
+
+public class BattleClassMod {
+	
+	@EventHandler
+	public static void preInit( FMLPreInitializationEvent event ){
+		
+	}
+
+	@EventHandler
+	public static void Init( FMLInitializationEvent event ){
+		
+	}
+	
+	@EventHandler
+	public static void postInit( FMLPostInitializationEvent event ){
+		
+	}
+	
 }
