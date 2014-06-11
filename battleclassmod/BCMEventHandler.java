62311e546da3b3fd0bcd003cded049b1b9cf265c
Malkuthe <jp.baltao@gmail.com>
2014-06-10 20:26:47 -0500

    Added player class to event handler

--- battleclassmod/BCMEventHandler.java
+++ battleclassmod/BCMEventHandler.java
@@ -1,5 +1,20 @@
 package battleclassmod;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
+
 public class BCMEventHandler {
 
+	@ForgeSubscribe
+	public void onEntityConstructing( EntityConstructing event ){
+		if ( event.entity instanceof EntityPlayer && PlayerClass.get( (EntityPlayer) event.entity ) == null){
+			PlayerClass.register( (EntityPlayer) event.entity );
+		}
+		
+		if ( event.entity instanceof EntityPlayer && event.entity.getExtendedProperties(PlayerClass.EXT_PROP_NAME) == null){
+			event.entity.registerExtendedProperties(PlayerClass.EXT_PROP_NAME, new PlayerClass((EntityPlayer) event.entity) );
+		}
+	}
+	
 }
