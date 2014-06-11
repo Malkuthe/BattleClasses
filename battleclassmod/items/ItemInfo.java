8ae12ee8ce3a07653b05b9e020fee0fcce7dde28
Malkuthe <jp.baltao@gmail.com>
2014-06-10 22:02:49 -0500

    Created Iteminfo index

---
+++ battleclassmod/items/ItemInfo.java
@@ -0,0 +1,13 @@
+package battleclassmod.items;
+
+public class ItemInfo {
+	
+	//Dummy item
+	
+	public static int dummyItemID;
+	public static final int dummyItemDefault = 24201;
+	public static final String dummyItemUnlocalized = "bcm.dummyitem";
+	public static final String dummyItemLocalized = "Dummy Item";
+	
+
+}
