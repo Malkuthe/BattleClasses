4ffd68abce8f05b7b7498a540045657af2830ae6
Malkuthe <jp.baltao@gmail.com>
2014-06-11 08:07:30 -0500

    Updated Event Handler for packets

--- battleclassmod/BCMEventHandler.java
+++ battleclassmod/BCMEventHandler.java
@@ -3,6 +3,8 @@ package battleclassmod;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
 public class BCMEventHandler {
 
@@ -14,6 +16,25 @@ public class BCMEventHandler {
 		
 		if ( event.entity instanceof EntityPlayer && event.entity.getExtendedProperties(PlayerClass.EXT_PROP_NAME) == null){
 			event.entity.registerExtendedProperties(PlayerClass.EXT_PROP_NAME, new PlayerClass((EntityPlayer) event.entity) );
+		}
+	}
+	
+	@ForgeSubscribe
+	public void onLivingDeathEvent( LivingDeathEvent event ){
+		if ( !event.entity.worldObj.isRemote && event.entity instanceof EntityPlayer ){
+			
+			PlayerClass.saveProxyData((EntityPlayer) event.entity);
+			
+		}
+		
+	}
+	
+	@ForgeSubscribe
+	public void onEntityJoinWorld( EntityJoinWorldEvent event ){
+		if ( !event.entity.worldObj.isRemote && event.entity instanceof EntityPlayer ){
+			
+			PlayerClass.loadProxyData((EntityPlayer) event.entity);
+			
 		}
 	}
 	
